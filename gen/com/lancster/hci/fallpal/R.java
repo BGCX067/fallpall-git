24cccfc132d62836027f66622565237397ac64f2
Carol <Carol@Carol-PC.fritz.box>
2014-12-01 15:51:56 +0000

    create new menu working list working

--- gen/com/lancster/hci/fallpal/R.java
+++ gen/com/lancster/hci/fallpal/R.java
@@ -22,13 +22,19 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_new=0x7f020000;
+        public static final int ic_action_settings=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
+        public static final int action_settings=0x7f090007;
+        public static final int add_contact=0x7f090008;
+        public static final int add_existing_button=0x7f090004;
+        public static final int add_new_button=0x7f090003;
+        public static final int add_new_layout=0x7f090002;
         public static final int contactList=0x7f090001;
-        public static final int contactName=0x7f090002;
-        public static final int contactStatus=0x7f090003;
+        public static final int contactName=0x7f090005;
+        public static final int contactStatus=0x7f090006;
         public static final int mainTitle=0x7f090000;
     }
     public static final class integer {
@@ -38,13 +44,18 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int clistitem=0x7f030001;
+        public static final int add_new_popup_layout=0x7f030001;
+        public static final int clistitem=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f060002;
+        public static final int add_create_existing=0x7f060006;
+        public static final int add_create_new=0x7f060005;
+        public static final int add_dialog_title=0x7f060007;
+        public static final int add_new=0x7f060004;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060001;
         public static final int welcome_header=0x7f060003;
--- src/com/lancster/hci/fallpal/MainActivity.java
+++ src/com/lancster/hci/fallpal/MainActivity.java
@@ -1,41 +1,84 @@
 package com.lancster.hci.fallpal;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
-import android.app.Activity;
+import android.app.FragmentManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 	
 	private ArrayList<Contact> myContacts; 
+	private BroadcastReceiver bRec = new BroadcastReceiver() {
+		
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			Toast.makeText(MainActivity.this, "selected item " + intent.getExtras().getInt("actionValue"), Toast.LENGTH_SHORT).show();
+		}
+	};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        LocalBroadcastManager.getInstance(this).registerReceiver(bRec, new IntentFilter("createAction"));
         initContactList();
     }
 
+    
+    
     private void initContactList() {
     	myContacts = new ArrayList<Contact>();
+    	addTestEntries();
     	
     	ContactAdapter conAd = new ContactAdapter(this, myContacts);
         ListView contactList = (ListView) findViewById(R.id.contactList);
         contactList.setAdapter(conAd);
+        
+        contactList.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				
+
+				Toast.makeText(MainActivity.this, myContacts.get(position).getName(), Toast.LENGTH_SHORT).show();
+				
+			}
+        	
+		});
+        registerForContextMenu(contactList);
+    }
+    
+    private void addTestEntries() {
+    	myContacts.add(new Contact("Adi"));
+    	myContacts.add(new Contact("Cuthbert"));
+    }
+    
+    public void addContact(String name) {
+    	myContacts.add(new Contact(name));
+    	
     }
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -44,12 +87,27 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        
+        switch (id) {
+		case R.id.action_settings:
+			Toast.makeText(MainActivity.this, "GoTo Settings", Toast.LENGTH_SHORT).show();
             return true;
-        }
-        return super.onOptionsItemSelected(item);
+            
+		case R.id.add_contact:
+			showAddPopup();
+			return true;
+			
+		default:
+			return true;
+		}
     }
 
+
+	private void showAddPopup() {
+		AddNewDialog andy = new AddNewDialog();
+		FragmentManager fm = getFragmentManager();
+		andy.show(fm, "itsmee");
+	}
 
 	public ArrayList<Contact> getMyContacts() {
 		return myContacts;
