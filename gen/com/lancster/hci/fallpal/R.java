7da6339c9aef0670899fa4a74f7da7c8ad115d4b
Carol <Carol@Carol-PC>
2014-12-04 00:00:32 +0000

    Layout of first setup screen. ToDo: - Make different fragment layouts for following steps - write popup to select phone contacts - save entered data to local database

--- gen/com/lancster/hci/fallpal/R.java
+++ gen/com/lancster/hci/fallpal/R.java
@@ -25,20 +25,27 @@ public final class R {
         public static final int ic_action_add_person=0x7f020000;
         public static final int ic_action_discard=0x7f020001;
         public static final int ic_action_new=0x7f020002;
-        public static final int ic_action_save=0x7f020003;
-        public static final int ic_action_settings=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
+        public static final int ic_action_person=0x7f020003;
+        public static final int ic_action_save=0x7f020004;
+        public static final int ic_action_settings=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
-        public static final int add_contact=0x7f090007;
+        public static final int action_settings=0x7f09000c;
+        public static final int add_contact=0x7f09000d;
         public static final int contactList=0x7f090001;
         public static final int contactName=0x7f090003;
         public static final int contactStatus=0x7f090004;
-        public static final int editText1=0x7f090005;
+        public static final int editText1=0x7f090006;
+        public static final int editText2=0x7f090008;
+        public static final int imageButton1=0x7f09000b;
         public static final int mainTitle=0x7f090000;
-        public static final int save_new_contact=0x7f090008;
+        public static final int phoneField=0x7f09000a;
+        public static final int save_new_contact=0x7f09000e;
         public static final int setupViewPager=0x7f090002;
+        public static final int textView1=0x7f090005;
+        public static final int textView2=0x7f090007;
+        public static final int textView3=0x7f090009;
     }
     public static final class integer {
         public static final int STATUS_FALL=0x7f050002;
